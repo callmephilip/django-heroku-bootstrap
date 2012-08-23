0412644f2356629ab88ec2920c4e52f734baddec
Philip Nuzhnyi <jekyll@Philips-MacBook-Air.local>
2012-08-23 16:23:57 -0700

    default location of the dev database changed - project root

--- settings/dev.py
+++ settings/dev.py
@@ -6,6 +6,6 @@ INTERNAL_IPS = ["127.0.0.1"]
 
 STATIC_URL = '/static/'
 
-DATABASES = {'default' : {'ENGINE' : 'django.db.backends.sqlite3', 'NAME' : os.path.join(os.path.dirname(__file__),'data.db')}}
+DATABASES = {'default' : {'ENGINE' : 'django.db.backends.sqlite3', 'NAME' : os.path.join(os.path.dirname(__file__),'../data.db')}}
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
