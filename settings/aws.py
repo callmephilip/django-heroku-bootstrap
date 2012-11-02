47963036babc5375dc4dab569506880659a29738
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2012-11-01 23:32:40 -0700

    Amazon Web Services settings moved to a separate file

---
+++ settings/aws.py
@@ -0,0 +1,13 @@
+# AWS settings
+
+#Your Amazon Web Services access key, as a string.
+AWS_ACCESS_KEY_ID = ""
+
+#Your Amazon Web Services secret access key, as a string.
+AWS_SECRET_ACCESS_KEY = ""
+
+#Your Amazon Web Services storage bucket name, as a string.
+AWS_STORAGE_BUCKET_NAME = ""
+
+#Additional headers to pass to S3
+AWS_HEADERS = {}
