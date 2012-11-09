8b2ba8f8a60c5cbda9418bd9a021378c8c50be51
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2012-11-08 16:29:36 -0800

    useful routines used for housekeeping

---
+++ tools/heroku.py
@@ -0,0 +1,19 @@
+import os, re
+import dj_database_url
+
+def database_config(env_varibale_pattern="HEROKU_POSTGRESQL_\S+_URL", default_env_variable="DATABASE_URL"):
+
+    r = re.compile(env_varibale_pattern)
+
+    urls = filter(lambda k : r.match(k) is not None, os.environ.keys())
+
+    if len(urls) > 1:
+        if not os.environ.has_key(default_env_variable):
+            print "Multiple env variables matching %s detected. Using %s" % (env_varibale_pattern, urls[0])
+
+
+    if len(urls) == 0:
+        if not os.environ.has_key(default_env_variable):
+            raise Exception("No database detected. Make sure you enable database on your heroku instance (e.g. heroku addons:add heroku-postgresql:dev)")
+
+    return dj_database_url.config(default_env_variable, os.environ[urls[0]] if len(urls) !=0 else None)
